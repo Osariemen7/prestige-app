c7c8f8093b5f02b1076a4ccba841db73532ee0dc
Osariemen Aibueku <102379269+Osariemen7@users.noreply.github.com>
2023-11-25 11:23:20 +0100

    Add files via upload

--- src/components/customer.jsx
+++ src/components/customer.jsx
@@ -65,7 +65,7 @@ if(loading) {
               //const spaceIndex = list[0].customers[0].customer_name.indexOf(' ');
               const customer=(index)=>{
                 const data = list[0].customers[index]
-                 navigate('/components/cusdet', {state:{data}})
+                 navigate('/components/customanalytics', {state:{data}})
               }
                     
     return(
