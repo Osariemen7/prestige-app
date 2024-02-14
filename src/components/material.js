1802688ed046352852cfa27e696914690784de29
Osariemen Aibueku <102379269+Osariemen7@users.noreply.github.com>
2024-02-14 02:47:05 +0100

    Add files via upload

--- src/components/material.js
+++ src/components/material.js
@@ -42,6 +42,30 @@ export const BootstrapButton = styled(Button)({
       boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
     },
   });
+
+  export const CssTextField = styled(TextField)({
+    '& label.Mui-focused': {
+      color: '#0096FF',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: '#0096FF',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#E0E3E7',
+      },
+      '&:hover fieldset': {
+        borderColor: '#B2BAC2',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#6F7E8C',
+      },
+    },
+    width: '88%',
+    maxWidth: '100%' 
+    
+  });
+   
   
  export const ColorButton = styled(Button)(({ theme }) => ({
     color: theme.palette.getContrastText(purple[500]),
@@ -67,6 +91,26 @@ export const BootstrapButton = styled(Button)({
     },
     width: '88%',
     maxWidth: '100%'
+    
+  });
+  
+  export const ValidationEyeField = styled(TextField)({
+    '& input:valid + fieldset': {
+      borderColor: '#E0E3E7',
+      borderWidth: 1,
+      padding: '5px'
+    },
+    '& input:invalid + fieldset': {
+      borderColor: 'red',
+      borderWidth: 1,
+    },
+    '& input:valid:focus + fieldset': {
+      borderLeftWidth: 4,
+      padding: '6px !important', // override inline-style
+    },
+    width: '88%',
+    maxWidth: '100%',
+    marginLeft: '5%'
     
   });
   
