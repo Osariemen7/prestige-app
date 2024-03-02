baa2e20d6ff17a7db1d51d0dcb540fcc1ce91142
Osariemen Aibueku <102379269+Osariemen7@users.noreply.github.com>
2024-03-02 23:09:23 +0100

    Add files via upload

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react"
 import { Helmet } from "react-helmet";
 import { Link, useNavigate } from "react-router-dom"
-import Modal from 'react-modal'
 import { Typography, TextField } from '@mui/material';
 import { BootstrapButton, ValidationTextField, CssTextField, ValidationEyeField} from './material.js'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
+import { useDisclosure, Button, Input, ChakraProvider } from "@chakra-ui/react"
 
 
 
@@ -16,6 +25,8 @@ const LoginPage = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [email, setEmail] = useState('')
     const navigate = useNavigate()
+    const modal2 = useDisclosure()
+  
 
     const openModal = () => {
       setIsOpen(true);
@@ -107,12 +118,14 @@ const LoginPage = () => {
       }
    
     return(
-        <div style={{padding: '4%'}}>
+        <div style={{padding: '4%', marginTop: '3%'}}>
         <Helmet>
             <title>Log in Page</title>
             
         </Helmet>
-            <h2 className="lh">Welcome Back</h2> 
+        <Typography fontSize='30px' marginLeft='5%' fontWeight='600' className="lh" gutterBottom>
+        Welcome Back
+      </Typography>
             <p className="lp">Let's get started with some basic information about your business </p>
 
            <br/>
@@ -144,26 +157,36 @@ const LoginPage = () => {
       </BootstrapButton>
 
                 <div className="message">{message ? <p>{message}</p> : null}</div>
-                <p className="lop" onClick={openModal}>Forgot Password?</p>
+                <p className="lop" onClick={modal2.onOpen}>Forgot Password?</p>
             </form>
             <footer className="fot">Dont have an account? <Link to='/components/signup'><span className="lsf">Sign Up</span></Link></footer>
-            <Modal
-      className='modal'
-      isOpen={isOpen}
-      onRequestClose={closeModal}
-      contentLabel="Example Popup"
-    >
-         <i className="fa-solid fa-x mx" onClick={closeModal}></i>
-         
-           <p >Please Enter Your Email</p>
-
-           <form>
-              <p className="sp">Email Address</p>
-              <input className="lin" type="email" onChange={handleEmailChange} name="email" placeholder="Enter Email Address" required/><br/><br/>
-              <button className="logb" onClick={ema} type="submit">Next</button>
-              <div className="message">{messag ? <p>{messag}</p> : null}</div>
-           </form>
-    </Modal>
+            
+            <ChakraProvider>
+            <Modal isOpen={modal2.isOpen} onClose={modal2.onClose}>
+        <ModalOverlay />
+        <ModalContent>
+  
+          <ModalHeader>Confirm Email</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody> 
+      
+      <div>
+      <h4 className='h4'></h4>
+      <form>
+      <Input placeholder='Email Address' size='md' type="email" onChange={handleEmailChange} width={273} ml={9}/><br/><br/>
+     
+        <br />
+        <div className="message">{messag ? <p>{messag}</p> : null}</div>
+          
+                <Button colorScheme='blue' onClick={ema}>Next</Button>
+            </form>
+            </div>
+            
+            </ModalBody>
+              </ModalContent>
+        </Modal>
+            </ChakraProvider>
+           
         </div>
     )
  }
